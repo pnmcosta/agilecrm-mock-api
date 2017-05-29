48638b35d9f37d938ed8303805220980a91ea3c4
Pedro Maia Costa <geral@pmcdigital.pt>
2017-05-30 00:00:28 +0100

    remove not needed merge

--- merge.bat
+++
@@ -1,5 +0,0 @@
-git remote add vsts https://$(System.AccessToken)@pmcdigital.visualstudio.com/DefaultCollection/PMC/_git/agilecrm-mock-api
-git checkout -b vsts-merge
-git pull vsts master
-rm merge.bat
-git push origin vsts-merge:master
